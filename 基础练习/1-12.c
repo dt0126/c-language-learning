d3bc20f982745667b9e79ec82ee4710df39ee30f
dt0126 <729090533@qq.com>
2025-11-26 16:50:45 +0800

    Add files via upload
    水平直方图

--- 基础练习/1-12.c
+++ 基础练习/1-12.c
@@ -1,16 +1,43 @@
 #include<stdio.h>
 
+#define MAX_SIZE 20
+
 int main()
 {
 	int c;
-
+	int WordQuency[MAX_SIZE] = { 0 };
+	int count_WordQuency=0;
+	int count_WordSize = 0;
 
 	while ((c = getchar()) != EOF)
 	{
-		putchar(c);
+	
+		if (c == '\n' || c == ' ' || c == "\t")
+		{
+			WordQuency[count_WordSize]++;
+
+			count_WordSize = 0;
+	
+		}
+		else if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		{
+			count_WordSize++;
+		}
+		
+	}
+	printf("开始绘制直方图：");
+	system("cls");
+
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		printf("长度为的%2d单词出现次数的直方图：", i+1);
+		while (WordQuency[i] > 0)
+		{
+			printf("※");
+			WordQuency[i]--;
+		}
 		printf("\n");
 	}
-
 	
 	return 0;
 }
